823aedcb14fb4c08dbb2f5d4f8dab23981af8c59
GTech20 <ditchon16@gmail.com>
2019-01-10 15:31:35 +0800

    first

---
+++ table.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <body>
+    <?php
+        $a=1000;
+        $b=1200;
+        $c=1400;
+                echo"<table border=1 cellspacing=0><tr><td>
+                <font color=blue> Salary of Mr. A is </td <td>$a$</font></td></tr>
+                <font color=blue> Salary of Mr. B is </td <td>$b$</font></td></tr>
+                <font color=blue> Salary of Mr. C is </td <td>$c$</font></td></tr>
+                </table>"
+    ?>
+    </body>
+</html>
+
